8a137339d09f97c12b484a5ff5b3f8c9efa83834
Carl Higgs <carl.higgs@rmit.edu.au>
2026-01-08 12:29:49 +1100

    updated health exposure model for Melbourne to use Fast Dijkstra algorithm for routing, as per #175

--- useCases/melbourne/src/main/java/de/tum/bgu/msm/health/HealthExposureModelMEL.java
+++ useCases/melbourne/src/main/java/de/tum/bgu/msm/health/HealthExposureModelMEL.java
@@ -33,6 +33,7 @@ import org.matsim.core.config.Config;
 import org.matsim.core.controler.ControlerDefaults;
 import org.matsim.core.network.NetworkUtils;
 import org.matsim.core.population.PopulationUtils;
+import org.matsim.core.router.FastMultiNodeDijkstraFactory;
 import org.matsim.core.router.speedy.SpeedyALTFactory;
 import org.matsim.core.router.util.LeastCostPathCalculator;
 import org.matsim.core.router.util.TravelDisutility;
@@ -852,7 +853,9 @@ public class HealthExposureModelMEL extends AbstractModel implements ModelUpdate
         AtomicInteger NO_PATH_TRIP = new AtomicInteger();
 
         for (final List<Trip> partition : partitions) {
-            LeastCostPathCalculator pathCalculator = new SpeedyALTFactory().createPathCalculator(scenario.getNetwork(), travelDisutility, travelTime);
+            LeastCostPathCalculator pathCalculator = new FastMultiNodeDijkstraFactory().createPathCalculator(
+                    scenario.getNetwork(), travelDisutility, travelTime
+            );
             PopulationFactory factory = PopulationUtils.getFactory();
 
             executor.addTaskToQueue(() -> {
