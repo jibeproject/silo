3a7ccd2d05afa034ac24931f0d675905b1b4921f
Carl Higgs <carl.higgs@rmit.edu.au>
2026-05-19 16:24:08 +1000

    added in RunAirPollutantModel.java for Melbourne (code by @ismailsaadi) for #191 / #192

---
+++ useCases/melbourne/src/main/java/de/tum/bgu/msm/health/RunAirPollutantModel.java
@@ -0,0 +1,67 @@
+package de.tum.bgu.msm.health;
+
+import de.tum.bgu.msm.data.Day;
+import de.tum.bgu.msm.data.Zone;
+import de.tum.bgu.msm.data.person.Gender;
+import de.tum.bgu.msm.data.person.Person;
+import de.tum.bgu.msm.health.airPollutant.AirPollutantModel;
+import de.tum.bgu.msm.health.data.LinkInfo;
+import de.tum.bgu.msm.health.data.ActivityLocation;
+import de.tum.bgu.msm.health.data.PersonHealth;
+import de.tum.bgu.msm.health.disease.Diseases;
+//import de.tum.bgu.msm.health.io.CarDriverShareTableReader;
+import de.tum.bgu.msm.health.io.PrevalenceDataReader;
+import de.tum.bgu.msm.health.noise.NoiseModel;
+import de.tum.bgu.msm.properties.Properties;
+import de.tum.bgu.msm.resources.Resources;
+import de.tum.bgu.msm.utils.SiloUtil;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.MutableScenario;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import de.tum.bgu.msm.health.injury.AccidentModel;
+
+import java.io.IOException;
+import java.util.*;
+
+/**
+ * Analyze AP model outputs
+ *
+ * @author Ismail Saadi*/
+
+
+public class RunAirPollutantModel {
+
+    private final static Logger logger = LogManager.getLogger(RunAirPollutantModel.class);
+
+    public static void main(String[] args) throws IOException {
+
+        Properties properties = SiloUtil.siloInitialization(args[0]);
+
+        // todo: check if that is good practice/ necessary to run the accident model, but need to make sure there are no implications elsewhere
+        Resources.initializeResources(properties.transportModel.mitoPropertiesPath);
+
+        Config config = null;
+        if (args.length > 1 && args[1] != null) {
+            config = ConfigUtils.loadConfig(args[1]);
+        }
+        logger.info("Started SILO land use model for the Greater Manchester");
+        HealthDataContainerImpl dataContainer = DataBuilderHealth.getModelDataForMelbourne(properties, config);
+        DataBuilderHealth.read(properties, dataContainer, config);
+
+        // AP
+        AirPollutantModel airPollutantModel = new AirPollutantModel(dataContainer, properties, SiloUtil.provideNewRandom(), config);
+        airPollutantModel.endYear(2021);
+
+        // Noise
+        //NoiseModel noiseModel = new NoiseModel(dataContainer, properties, SiloUtil.provideNewRandom(), config);
+        //noiseModel.endYear(2021);
+
+        logger.info("Finished analysis.");
+    }
+}
